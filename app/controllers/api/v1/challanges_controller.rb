e3f7272ef0e2d6f495421833ae97167915dfb126
Danish Rashed <danishrashed@live.com>
2025-03-11 01:13:50 +0500

    changes

--- app/controllers/api/v1/challanges_controller.rb
+++ app/controllers/api/v1/challanges_controller.rb
@@ -2,7 +2,11 @@ module Api
   module V1
     class ChallangesController < ApplicationController
 
-      def index;end
+      def index
+        challanges = Challange.all
+        render json: challanges
+      end
+      
       def create;end
       def show;end
       def update;end
