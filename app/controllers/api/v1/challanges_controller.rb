b2325cc5bb867b2df83728e8f9fc34d04a3abf0e
Danish Rashed <danishrashed@live.com>
2025-03-17 17:14:50 +0500

    changes

--- app/controllers/api/v1/challanges_controller.rb
+++ app/controllers/api/v1/challanges_controller.rb
@@ -7,10 +7,24 @@ module Api
         render json: challanges
       end
       
-      def create;end
-      def show;end
+      def create
+        challange = Challange.new(challange_params)
+        if challange.save
+          render json: challange, status: :created
+        else
+          render json: { errors: challange.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def show 
+      end
       def update;end
       def destroy;end
+
+      private
+      def challange_params
+        params.require(:challange).permit(:title, :description, :start_date, :end_date)
+      end
     end
   end
 end
