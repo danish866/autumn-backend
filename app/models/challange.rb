ef5d93bb8a97d8fdea011fb329d7e2d5d51288a9
Danish Rashed <danishrashed@live.com>
2025-03-27 01:13:50 +0500

    changes

--- app/models/challange.rb
+++ app/models/challange.rb
@@ -1,7 +1,8 @@
 class Challange < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
-  
-  validates :description, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
 
 end
