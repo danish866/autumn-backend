f83afb045f69bdd0d63c9f75d6d124df3833b864
Danish Rashed <danishrashed@live.com>
2025-04-09 01:13:50 +0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+    }
+  end
+
   namespace :api do
     namespace :v1 do
       resources :challanges
