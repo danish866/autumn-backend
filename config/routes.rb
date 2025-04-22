1444dd476d879394a44b54eb62a313f9a29937bb
Danish Rashed <danishrashed@live.com>
2025-04-22 15:04:23 +0500

    added jwt token for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations',
-    }
-  end
+  devise_for :users, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  
 
   namespace :api do
     namespace :v1 do
